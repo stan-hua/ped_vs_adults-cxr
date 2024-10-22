1e5d1791b8a359dedd089a1f5722b2430801977e
stan-hua <stanley.z.hua@gmail.com>
2024-10-22 15:41:15 -0400

    Modify constants

--- configs/constants.py
+++ configs/constants.py
@@ -20,14 +20,32 @@ SEED = 42
 ################################################################################
 # Project directories to load data (images, metadata)
 DIR_HOME = os.environ["HOME"]
-DIR_PROJECT = dirname(dirname(dirname(__file__)))
+DIR_PROJECT = dirname(dirname(__file__))
+
+# Path to data directory, containing CXR data
+DIR_DATA = join(DIR_PROJECT, "data")
+DIR_DATA_MAP = {
+    "save": join(DIR_DATA, "save_data"),
+    "metadata": join(DIR_DATA, "metadata"),
+    "vindr_cxr": join(DIR_DATA, "cxr_datasets", "vindr-cxr"),
+    "vindr_pcxr": join(DIR_DATA, "cxr_datasets", "vindr-pcxr"),
+}
+DIR_METADATA_MAP = {
+    "vindr_cxr": join(DIR_DATA_MAP["metadata"], "vindr_cxr_metadata.csv"),
+    "vindr_pcxr": join(DIR_DATA_MAP["metadata"], "vindr_pcxr_metadata.csv"),
+}
+
+# Directory containing configurations
+DIR_CONFIG = join(DIR_PROJECT, "config")
+# Directory containing config specifications
+DIR_CONFIG_SPECS = join(DIR_CONFIG, "configspecs")
 
 # Local directory to save data (model weights, embeddings, figures)
-DIR_SAVE = join(DIR_PROJECT, "src", "data")
-DIR_TRAIN_RUNS = join(DIR_SAVE, "train_runs")
-DIR_INFERENCE = join(DIR_SAVE, "inference")
-DIR_EMBEDS = join(DIR_SAVE, "embeddings")
-DIR_FIGURES = join(DIR_SAVE, "figures")
+DIR_SAVE_DATA = join(DIR_PROJECT, "data", "save_data")
+DIR_TRAIN_RUNS = join(DIR_SAVE_DATA, "train_runs")
+DIR_INFERENCE = join(DIR_SAVE_DATA, "inference")
+DIR_EMBEDS = join(DIR_SAVE_DATA, "embeddings")
+DIR_FIGURES = join(DIR_SAVE_DATA, "figures")
 
 # Directory containing eda figures
 DIR_FIGURES_EDA = join(DIR_FIGURES, "eda")
@@ -38,8 +56,9 @@ DIR_FIGURES_CAM = join(DIR_FIGURES, "grad_cam")
 # Directory containing prediction-related figures
 DIR_FIGURES_PRED = join(DIR_FIGURES, "predictions")
 
-# Directory containing configurations
-DIR_CONFIG = join(DIR_PROJECT "config")
-# Directory containing config specifications
-DIR_CONFIG_SPECS = join(DIR_CONFIG, "configspecs")
 
+################################################################################
+#                                 Data Related                                 #
+################################################################################
+# Image size
+IMG_SIZE = (512, 512)
